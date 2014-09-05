356d0fb0b429374328bf1378979ed8227932ceaf
Ruofei Xu <ruofei.xu@email.wsu.edu>
2014-09-05 01:40:27 -0700

    hw done!

---
+++ Hw1/Hw1/BST.cs
@@ -0,0 +1,169 @@
+﻿// Author:
+//    Ruofei Xu
+// Class:
+//    A BST tree class and BST node class
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw1
+{
+    //create a BST node class for BST 
+    public class BSTNode
+    {
+        public int Data;
+        public BSTNode Left;
+        public BSTNode Right;
+
+        public BSTNode(int data)
+        {
+            Data = data;
+            Left = null;
+            Right = null;
+        }
+        
+    }
+
+    //BST
+    public class BST
+    {
+        //declare the root and a counter variable
+        public BSTNode root;
+        public int counter;
+
+        public BST()
+        {
+            root = null;
+            counter = 0;
+        }
+
+        //Add method
+        public void Add(int value)
+        {
+            //add on the root if there is no node
+            if(null == root)
+            {
+                root = new BSTNode(value);
+                counter++;
+            }
+            else
+            {
+                BSTNode current_node = root;
+                while(true)
+                {
+                    //if the value already existed, then skip this value
+                    if(value == current_node.Data)
+                    {
+                        return;
+                    }
+                    //if the value smaller than current node and left node is null, then add this value to left
+                    else if(value < current_node.Data && null == current_node.Left)
+                    {
+                        BSTNode new_node = new BSTNode(value);
+                        current_node.Left = new_node;
+                        counter++;
+                        return;
+                    }
+                    //if the value larger then curren node and right node is null, then add this value to right
+                    else if(value > current_node.Data && null == current_node.Right)
+                    {
+                        BSTNode new_node = new BSTNode(value);
+                        current_node.Right = new_node;
+                        counter++;
+                        return;
+                    }
+                    //if the value smaller than curent node replace current node to left node
+                    else if(value < current_node.Data)
+                    {
+                        current_node = current_node.Left;
+                    }
+                    //if the value larger than curent node replace current node to right node
+                    else if(value > current_node.Data)
+                    {
+                        current_node = current_node.Right;
+                    }
+                    //just in case for debug
+                    else
+                    {
+                        Console.WriteLine("Unexpected result.");
+                        return;
+                    }
+                }
+            }
+        }
+
+        //get tree's height method
+        public int GetHeight(BSTNode current_node)
+        {
+            //if no node then height is 0
+            if(null == root)
+            {
+                return 0;
+            }
+            //if leaf node then height is 1
+            else if(null == current_node.Left && null == current_node.Right)
+            {
+                return 1;
+            }
+            //recursively find the height
+            else if(null == current_node.Left)
+            {
+                return GetHeight(current_node.Right) + 1;
+            }
+            else if(null == current_node.Right)
+            {
+                return GetHeight(current_node.Left) + 1;
+            }
+            else
+            {
+                return Math.Max(GetHeight(current_node.Left) + 1, GetHeight(current_node.Right));
+            }
+        }
+
+        //a method for printing value in ascending order
+        public void PrintInAscending(BSTNode current_node)
+        {
+            //print this infromation if just start
+            if(root.Data == current_node.Data)
+            {
+                Console.Write("Tree contents: ");
+            }
+            //print the value if it's a leaf node
+            if(null == current_node.Left && null == current_node.Right)
+            {
+                Console.Write("{0} ", current_node.Data);
+                return;
+            }
+            //recursively print all int in ascending
+            else if(null == current_node.Left)
+            {
+                Console.Write("{0} ", current_node.Data);
+                PrintInAscending(current_node.Right);
+                return;
+            }
+            else if(null == current_node.Right)
+            {
+                PrintInAscending(current_node.Left);
+                Console.Write("{0} ", current_node.Data);
+                return;
+            }
+            else
+            {
+                PrintInAscending(current_node.Left);
+                Console.Write("{0} ", current_node.Data);
+                PrintInAscending(current_node.Right);
+                return;
+            }
+        }
+
+        // a method for getting the lowest height
+        public int GetLowestHeight()
+        {
+            return (int)Math.Ceiling(Math.Log(counter,2));
+        }
+    }
+
+}
--- Hw1/Hw1/Program.cs
+++ Hw1/Hw1/Program.cs
@@ -1,8 +1,19 @@
-﻿using System;
+﻿// Author:
+//    Ruofei Xu
+/*
+ * Created by SharpDevelop.
+ * User: Ruofei
+ * Date: 8/29/2014
+ * Time: 1:28 PM
+ * 
+ * 
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace Hw1
 {
@@ -10,6 +21,32 @@ namespace Hw1
     {
         static void Main(string[] args)
         {
+            //declare a tree and a string for input
+            BST bst_tree = new BST { };
+            string line = string.Empty;
+
+            //get input number and split by space
+            Console.Write("Enter a collection of numbers in the range [0,100], separated by spaces: ");
+            line = Console.ReadLine();
+            string[] num_arr = line.Split(' ');
+
+            //add each int to the tree
+            foreach(string num_str in num_arr)
+            {
+                int new_num = 0;
+                new_num = Convert.ToInt32(num_str);
+                bst_tree.Add(new_num);
+            }
+
+            //output the expected result
+            bst_tree.PrintInAscending(bst_tree.root);
+            Console.WriteLine("\nTree statistics:");
+            Console.WriteLine("  Number of nodes: {0}", bst_tree.counter);
+            Console.WriteLine("  Number of levers: {0}", bst_tree.GetHeight(bst_tree.root));
+            Console.WriteLine("  Minimum number of levels that a tree with {0} nodes could have = {1}", bst_tree.counter, bst_tree.GetLowestHeight());
+            Console.WriteLine("Done");
+            Console.ReadKey();
+            return;
         }
     }
 }
